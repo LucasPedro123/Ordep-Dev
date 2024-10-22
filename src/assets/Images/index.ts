885575ba0901f6568894247c08162617f232bedc
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-22 00:54:13 -0300

    feat: add image exports for logos and service icons

---
+++ src/assets/Images/index.ts
@@ -0,0 +1,24 @@
+// Logo
+export { default as LogoWhite } from './logo-icon-white.svg'
+export { default as LogoBlue } from './logo-icon-blue.svg'
+
+// Hero
+export { default as HeroImage } from './hero-image.svg'
+
+// IconCards
+export {default as IconCardDocumentation} from './icon-documentation.svg'
+export { default as IconCardSupport } from './icon-support.svg'
+
+// Icon Services
+export { default as IconServiceMobile } from './icon-service-mobile.svg'
+export { default as IconServiceLandingpage } from './icon-service-landingpage.svg'
+export { default as IconServiceWeb } from './icon-service-web.svg'
+export { default as IconServiceDesktop } from './icon-service-desktop.svg'
+
+// Techs Icon
+export { default as IconTechNode } from './tech-icon-nodejs.svg'
+export { default as IconTechAspNet } from './tech-icon-aspnet.svg'
+export { default as IconTechReact } from './tech-icon-react.svg'
+export { default as IconTechFirebase } from './tech-icon-firebase.svg'
+export { default as IconTechAzure } from './tech-icon-azure.svg'
+export { default as IconTechMongodb} from './tech-icon-mongodb.svg'
