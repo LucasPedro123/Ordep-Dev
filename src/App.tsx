4353c1de7b5c24de8681a6ec02d85e60ffee6748
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-22 00:44:40 -0300

    feat: global style files added to App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { GlobalStyle } from "./assets/styles"
+import { ThemeProvider } from 'styled-components'
+import { ThemeGlobal } from "./assets/styles/theme"
+import Routes from './Routes/Routes.tsx'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={ThemeGlobal}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   )
 }
 
