026f1a81a86514907b5be9b954cb603f27616821
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-25 01:57:03 -0300

    refactor: changed the name of the button 1 classes

--- src/Components/Button1/index.tsx
+++ src/Components/Button1/index.tsx
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom';
 import './style.css'
 
-const Button1 = ({content, link} : {content: string, link : string}) => {
+export const Button1 = ({content, link} : {content: string, link : string}) => {
     
     return (
-        <button className='btn'>
-            <Link className='text-1' to={link}>{content}</Link>
-            <Link className='text-2' to={link}>{content}</Link>
+        <button className='btn1'>
+            <Link className='span-1' to={link}>{content}</Link>
+            <Link className='span-2' to={link}>{content}</Link>
         </button>    
     )
 }
-
-export default Button1;
