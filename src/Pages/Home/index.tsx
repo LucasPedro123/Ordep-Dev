f547c7204b2b19766fda392bd3275b9573f701a0
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-25 01:02:57 -0300

    feat: Added Info Cards to Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,44 @@
+import { IconCardDocumentation, IconCardSupport } from "../../assets/Images"
 import { Header } from "../../Components/Header"
+import Hero from "./Sections/Hero"
+import * as S from './style'
 
 export const Home: React.FC = () => {
+    const CardInfo = [{
+        title: 'Documentação Completa',
+        description: 'Receba documentação detalhada para entender e gerenciar seu projeto',
+        image: IconCardDocumentation
+    }, {
+        title: 'Suporte Garantido',
+        description: 'Tenha suporte contínuo para manutenção e atualizações futuras.',
+        image: IconCardSupport
+    }, {
+        title: 'Soluções Personalizadas',
+        description: 'Cada projeto é adaptado às necessidades e metas específicas da sua empresa.',
+        image: IconCardSupport
+    }]
+
     return (
         <>
-            <Header/>
+            <Header />
+            <Hero />
+            <S.Cards>
+                {
+                    CardInfo.map(item => (
+                        <S.Card>
+                            <S.CardImageContainer>
+                                <S.CardImage src={item.image} />
+                            </S.CardImageContainer>
+                            <S.CardWrapper>
+                                <S.CardTitle>{item.title}</S.CardTitle>
+                                <S.CardDescription>{item.description}</S.CardDescription>
+                            </S.CardWrapper>
+                        </S.Card>
+                    ))
+                }
+            </S.Cards>
+            <S.LineBg/>
+
         </>
-    )    
+    )
 }
