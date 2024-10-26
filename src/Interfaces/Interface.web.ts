5b7fc5f2a908d7cc88c45810abd60d504144dbc3
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-26 13:11:10 -0300

    chore: added properties to theme.ts object interface

--- src/Interfaces/Interface.web.ts
+++ src/Interfaces/Interface.web.ts
@@ -1,13 +1,35 @@
 // Theme interface
 export interface ITheme {
     colors: {
+        primary50: string;
         primary100: string;
         primary200: string;
+        primary300: string;
+        primary400: string;
+        primary500: string;
+        primary600: string;
+        primary700: string;
+        primary800: string;
+        primary900: string;
+        primary1000: string;
+        secondary50: string;
         secondary100: string;
         secondary200: string;
+        secondary300: string;
+        secondary400: string;
+        secondary500: string;
+        secondary600: string;
+        secondary700: string;
+        secondary800: string;
+        secondary900: string;
+        secondary1000: string;
         background: string;
         gray: string;
         white: string;
+        neutral50: string;
+        dark: string;
+        yellowLight: string;
+        blueLight: string;
     };
     fonts: {
         main: string;
