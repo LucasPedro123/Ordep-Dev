b01c73aec830975cfc81e746d900af71a8a7e60b
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-10-28 23:55:22 -0300

    feat: created Contacts section

---
+++ src/Pages/Home/Sections/Section-forms/index.tsx
@@ -0,0 +1,65 @@
+import { IconLocation } from '../../../../assets/Images'
+import * as S from './style'
+
+export const SectionForms: React.FC = () => {
+    return (
+        <S.Container>
+            <S.Content>
+                <S.Wrapper>
+                    <S.WrapperText>
+                        <S.Title>Vamos Conversar Mais</S.Title>
+                        <S.Description>Entre em contato para discutirmos suas ideias e como posso ajudar a transformá-las em soluções digitais. </S.Description>
+                    </S.WrapperText>
+                    <S.Cards>
+                        <S.Card>
+                            <S.ImageContainer>
+                                <S.ImageIcon src={IconLocation} />
+                            </S.ImageContainer>
+                            <S.CardWrapper>
+                                <S.CardTitle>Endereço:</S.CardTitle>
+                                <S.CardValue>Brasil, SP, São Paulo</S.CardValue>
+                            </S.CardWrapper>
+                        </S.Card>
+                        <S.Card>
+                            <S.ImageContainer>
+                                <S.ImageIcon src={IconLocation} />
+                            </S.ImageContainer>
+                            <S.CardWrapper>
+                                <S.CardTitle>E-mail:</S.CardTitle>
+                                <S.CardValue>lucaspedrof.dev@gmail.com</S.CardValue>
+                            </S.CardWrapper>
+                        </S.Card>
+                        <S.Card>
+                            <S.ImageContainer>
+                                <S.ImageIcon src={IconLocation} />
+                            </S.ImageContainer>
+                            <S.CardWrapper>
+                                <S.CardTitle>Telefone:</S.CardTitle>
+                                <S.CardValue>+55 (11) 97658-9382</S.CardValue>
+                            </S.CardWrapper>
+                        </S.Card>
+                    </S.Cards>
+                    <S.WrapperIcons>
+                        <S.Icon className='fa-brands fa-instagram'/>
+                        <S.Icon className='fa-brands fa-github'/>
+                        <S.Icon className='fa-brands fa-linkedin-in'/>
+                    </S.WrapperIcons>
+                </S.Wrapper>
+                <S.FormContainer>
+                    <S.FormDescription>Preencha o formulário abaixo e te responderei o mais breve possível.</S.FormDescription>
+                    <S.Form>
+                        <S.Input id='name' name='name' placeholder='Nome*' type='text'/>
+                        <S.Input id='email' name='email' placeholder='Email*' type='email'/>
+                        <S.Input id='location' name='location' placeholder='Localização*' type='text'/>
+                        <S.InputWrapper>
+                            <S.InputBudget id='budget' name='budget' placeholder='Orçamento*' type='number'/>
+                            <S.Input id='subject' name='subject' placeholder='Assunto*' type='text'/>
+                        </S.InputWrapper>
+                        <S.Input id='message' name='message' placeholder='Mensagem*' type='text'/>
+                    </S.Form>
+                    <S.Button>Enviar <S.IconButton/></S.Button>
+                </S.FormContainer>
+            </S.Content>
+        </S.Container>
+    )
+}
---
+++ src/Pages/Home/Sections/Section-forms/style.tsx
@@ -0,0 +1,199 @@
+import styled from 'styled-components';
+import { IconPapperSubmit } from '../../../../assets/Images';
+
+export const Container = styled.section`
+`
+export const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-inline: 5%;
+    padding: 88px;
+    gap: 24px;
+    background: ${({theme})=> theme.colors.white};
+    box-shadow: 0px 59px 124px rgba(0, 0, 0, 0.12);
+    border-radius: 15px;
+`
+
+export const Title = styled.h1`
+    font-weight: ${({theme})=> theme.fontWeight.bold};
+    font-size: ${({theme})=> theme.fontSizes.headline400};
+    line-height: 87px;
+    letter-spacing: -0.02em;
+    color: ${({theme})=> theme.colors.gray};
+`
+
+export const Description = styled.p`
+    font-weight: ${({theme})=> theme.fontWeight.regular};
+    font-size:  ${({theme})=> theme.fontSizes.paragraphP2};
+    line-height: 30px;
+    letter-spacing: -0.02em;
+    color: ${({theme})=> theme.colors.neutral50};
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 35px;
+    width: 431px;
+`
+
+export const WrapperText = styled.article`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
+export const Cards = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+export const Card = styled.div`
+    width: 336px;
+    height: 96px;
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: 0px 12px 64px rgba(28, 25, 25, 0.12);
+    border-radius: 10px;
+    padding: 24px;
+    display: flex;
+    gap: 13px;
+`
+
+export const CardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+
+export const ImageContainer = styled.div`
+    height: 100%;
+    border-radius: 4px;
+    background: ${({theme})=> theme.colors.primary500};
+`
+
+export const ImageIcon = styled.img`
+    padding: 13px 15px;
+`
+
+export const Icon = styled.i`
+    font-size: 24px;
+    color: ${({ theme }) => theme.colors.primary500};
+    transition: 300ms ease;
+
+    &:hover{
+        color: ${({ theme }) => theme.colors.primary600};
+    }
+`
+
+export const WrapperIcons = styled.div`
+    display: flex;
+    gap: 42px;
+`
+export const CardTitle = styled.p`
+    font-weight: ${({theme})=> theme.fontWeight.regular};
+    font-size: ${({theme})=> theme.fontSizes.paragraphP4};
+    color: ${({theme})=> theme.colors.neutral40};
+    line-height: 20px;
+`
+
+export const CardValue = styled.p`
+    font-weight: ${({theme})=> theme.fontWeight.medium};
+    font-size: ${({theme})=> theme.fontSizes.paragraphP3};
+    color: ${({theme})=> theme.colors.dark};
+`
+
+export const FormContainer = styled.div`
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+`
+
+export const FormDescription = styled.p`
+    font-size: ${({ theme }) => theme.fontSizes.paragraphP2};
+    color: ${({theme})=> theme.colors.neutral50};
+    font-weight: ${({theme})=> theme.fontWeight.regular};
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`
+
+export const Input = styled.input`
+    border: none;
+    padding-bottom: 14px;
+    width: 100%;
+    font-size:  ${({theme})=> theme.fontSizes.paragraphP2};
+    border-bottom: 1.5px solid ${({ theme }) => theme.colors.neutral50};
+
+    &:focus{
+        outline: none;
+        border-bottom: 1.5px solid ${({ theme }) => theme.colors.primary500};
+        
+        &::placeholder {
+            color: ${({ theme }) => theme.colors.primary500};
+        }
+    }
+
+    &::placeholder{
+        font-size:  ${({theme})=> theme.fontSizes.paragraphP2};
+    }
+`
+export const InputBudget = styled.input`
+    border: none;
+    padding-bottom: 14px;
+    width: 60%;
+    font-size:  ${({theme})=> theme.fontSizes.paragraphP2};
+    border-bottom: 1.5px solid ${({theme})=> theme.colors.neutral50};
+
+    &:focus{
+        outline: none;
+        border-bottom: 1.5px solid ${({ theme }) => theme.colors.primary500};
+        
+        &::placeholder {
+            color: ${({ theme }) => theme.colors.primary500};
+        }
+    }
+
+    &::placeholder{
+        font-size:  ${({theme})=> theme.fontSizes.paragraphP2};
+    }
+`
+
+export const InputWrapper = styled.div`
+    display: flex;
+    gap: 24px;
+    justify-content: space-between;
+`
+
+export const Button = styled.button`
+    width: 200px;
+    height: 60px;
+    border-radius: 55px;
+    background-color: ${({theme})=> theme.colors.primary500};
+    color: ${({theme})=> theme.colors.white};;
+    border-color: ${({theme})=> theme.colors.primary700};
+    font-size: ${({ theme }) => theme.fontSizes.paragraphP3};
+    font-weight: ${({theme})=> theme.fontWeight.bold};
+    transition: 500ms ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+
+    &:hover{
+        background-color: ${({theme})=> theme.colors.primary700};
+    }
+`
+
+export const IconButton = styled.div`
+    background-image: url(${IconPapperSubmit});
+    background-repeat: none;
+    background-size: 24px 24px;
+    width: 24px;
+    height: 24px;
+`
