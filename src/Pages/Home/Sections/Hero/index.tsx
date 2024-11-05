dbe0da51c74d3184767429c585d917acff49c430
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-04 23:25:42 -0300

    feat: add Framer Motion animations to the Hero section

--- src/Pages/Home/Sections/Hero/index.tsx
+++ src/Pages/Home/Sections/Hero/index.tsx
@@ -1,23 +1,37 @@
 import {MainImage } from '../../../../assets/Images';
 import {Button3} from '../../../../Components/Button';
 import * as S from './style';
+import {motion} from 'framer-motion'
 
 const Hero: React.FC = () => {
+
+    const Wrapper = motion(S.Wrapper)
+    const ImageContainer = motion(S.ImageContainer)
 
     return (
         <S.Container>
             <S.Content>
-                <S.Wrapper>
+                <Wrapper
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={{duration: 0.5}}
+                >
                     <S.Title>Torne ideias em<br /> realidade digital</S.Title>
                     <S.Description>
                         Desenvolvimento de software sob medida para dar vida às suas ideias, em apps
                         móveis, sites ou páginas otimizadas. Crie a solução ideal para o seu negócio.
                     </S.Description>
                     <Button3 content='Comece Agora' link='/'/>
-                </S.Wrapper>
-                <S.ImageContainer>
+                </Wrapper>
+                <ImageContainer
+                    initial={{ opacity: 0, y: 280, scale: 0.5 }}
+                    whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                    exit={{ scale: 0.5, y: 280, opacity: 0 }}
+                    transition={{duration: 0.8}}
+                >
                     <S.HeroImage src={MainImage} alt='Imagem principal' />
-                </S.ImageContainer>
+                </ImageContainer>
             </S.Content>
          
         </S.Container>
