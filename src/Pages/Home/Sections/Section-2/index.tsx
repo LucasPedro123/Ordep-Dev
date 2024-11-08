338410ca8aeedb4704c83ed91fde056d86fdc8a6
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-07 23:16:57 -0300

    feat: added animated to section 2 with Framer Motion

--- src/Pages/Home/Sections/Section-2/index.tsx
+++ src/Pages/Home/Sections/Section-2/index.tsx
@@ -1,11 +1,23 @@
+import { fadeInImage, fadeInUp } from '../../../../animations/animations.motion';
 import { SectionImage2 } from '../../../../assets/Images';
 import { Button2 } from '../../../../Components/Button';
 import * as S from './style';
 
 const SectionMobileSolution: React.FC = () => {
     return (
-        <S.Container>
-            <S.ImageContainer>
+        <S.Container
+            variants={fadeInUp}
+            initial={fadeInUp.initial}
+            whileInView={fadeInUp.animate}
+            exit={fadeInUp.exit}
+            transition={fadeInUp.transition}
+        >
+            <S.ImageContainer
+                initial={fadeInImage.initial}
+                whileInView={fadeInImage.animate}
+                exit={fadeInImage.exit}
+                transition={fadeInImage.transition}
+            >
                 <S.Image src={SectionImage2} />
             </S.ImageContainer>
             <S.Wrapper>
