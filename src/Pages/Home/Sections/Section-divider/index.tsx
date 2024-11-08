dcf4b0b3fad15abee0e95e7c4487b6f5a140339a
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-07 23:17:31 -0300

    feat: added animated to section Divider with Framer Motion

--- src/Pages/Home/Sections/Section-divider/index.tsx
+++ src/Pages/Home/Sections/Section-divider/index.tsx
@@ -1,3 +1,4 @@
+import { fadeInUp } from '../../../../animations/animations.motion'
 import { Button3 } from '../../../../Components/Button'
 import * as S from './style'
 
@@ -6,7 +7,13 @@ export const SectionDivider = () => {
 
     return (
         <S.Container>
-            <S.Wrapper>
+            <S.Wrapper
+                variants={fadeInUp}
+                initial={fadeInUp.initial}
+                whileInView={fadeInUp.animate}
+                exit={fadeInUp.exit}
+                transition={fadeInUp.transition}
+            >
                 <S.Title>Leve Suas Ideias para o <br/> Próximo Nível</S.Title>
                 <S.Description>Não importa onde você esteja, transformamos suas ideias em realidade digital, seja para um novo site, plataforma ou aplicativo. Com processos ágeis e comunicação direta, você acompanha cada etapa do desenvolvimento.</S.Description>
             </S.Wrapper>
