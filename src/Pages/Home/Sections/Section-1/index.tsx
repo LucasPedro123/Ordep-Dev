10f0741c9cbe01981e9593e6d89f49f00687e59e
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-07 23:16:40 -0300

    feat: added animated to section 1 with Framer Motion

--- src/Pages/Home/Sections/Section-1/index.tsx
+++ src/Pages/Home/Sections/Section-1/index.tsx
@@ -1,10 +1,17 @@
+import { fadeInImage, fadeInUp } from '../../../../animations/animations.motion';
 import { SectionImage1 } from '../../../../assets/Images';
 import {Button2} from '../../../../Components/Button';
 import * as S from './style';
 
 const SectionWebSolution: React.FC = () => {
     return (
-        <S.Container>
+        <S.Container
+            variants={fadeInUp}
+            initial={fadeInUp.initial}
+            whileInView={fadeInUp.animate}
+            exit={fadeInUp.exit}
+            transition={fadeInUp.transition}
+        >
             <S.Wrapper>
                 <S.Content>
                     <S.Title>Soluções  para Impulsionar Seu Negócio</S.Title>
@@ -12,7 +19,12 @@ const SectionWebSolution: React.FC = () => {
                 </S.Content>
                 <Button2 link='/' content='Saiba Mais'/>
             </S.Wrapper>
-            <S.ImageContainer>
+            <S.ImageContainer
+                initial={fadeInImage.initial}
+                whileInView={fadeInImage.animate}
+                exit={fadeInImage.exit}
+                transition={fadeInImage.transition}
+            >
                 <S.Image src={ SectionImage1} />
             </S.ImageContainer>
         </S.Container>
