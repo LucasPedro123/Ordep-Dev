00fb1baa109c472b4a536ade47f5378bca359919
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-20 16:21:31 -0300

    feat: added hyperlink in Footer component

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,7 +1,27 @@
-import { IconPapperSubmit, LogoWhite } from '../../assets/Images'
-import * as S from './style'
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { IconPapperSubmit, LogoWhite } from '../../assets/Images';
+import * as S from './style';
 
 export default function Footer() {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.hash === '#main') {
+            const mainSection = document.getElementById('main');
+            if (mainSection) {
+                mainSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [location]);
+
+    const links = [
+        { title: 'Home', path: '/#hero' },
+        { title: 'Sobre', path: '/about#main' },
+        { title: 'Serviços', path: '/#services' },
+        { title: 'Projetos', path: '/' }
+    ];
+
     return (
         <S.Container>
             <S.Content>
@@ -10,19 +30,23 @@ export default function Footer() {
                         <S.Logo src={LogoWhite} alt='Logo branca' loading="lazy" />
                         <S.Title>Ordep Dev</S.Title>
                     </S.Linked>
-                    <S.Description>Ordep Dev was created for the new ways we live and work. We make a better workspace around the world</S.Description>
+                    <S.Description>
+                        Ordep Dev was created for the new ways we live and work. We make a better workspace around the world
+                    </S.Description>
                 </S.WrapperText>
                 <S.Column>
                     <S.ColumnName>Redes</S.ColumnName>
-                    <S.Value to={'/'}>LinkedIn</S.Value>
-                    <S.Value to={'/'}>Instagram</S.Value>
-                    <S.Value to={'/'}>GitHub</S.Value>
+                    <S.Value target='_blank' to={'https://www.linkedin.com/in/lucas-pedro-fernandes/'}>LinkedIn</S.Value>
+                    <S.Value target='_blank' to={'https://www.instagram.com/lucasfernandes4150/'}>Instagram</S.Value>
+                    <S.Value target='_blank' to={'https://github.com/LucasPedro123'}>GitHub</S.Value>
                 </S.Column>
                 <S.Column>
                     <S.ColumnName>Links</S.ColumnName>
-                    <S.Value to={'/'}>Sobre</S.Value>
-                    <S.Value to={'/'}>Projetos</S.Value>
-                    <S.Value to={'/'}>Contato</S.Value>
+                    {
+                        links.map((e, i) => (
+                            <S.Value key={i} as={Link} to={e.path}>{e.title}</S.Value> // Usando Link para navegação do React Router
+                        ))
+                    }
                 </S.Column>
                 <S.WrapperForm>
                     <S.Title>Newsletter</S.Title>
@@ -37,5 +61,5 @@ export default function Footer() {
                 <S.Description2>© 2024.All Rights Reserved.</S.Description2>
             </S.Wrapper>
         </S.Container>
-    )
+    );
 }
