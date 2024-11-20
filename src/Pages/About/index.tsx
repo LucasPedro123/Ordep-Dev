37f47156e1c5e8e2a3ce355ae8101ae0f7637936
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-20 20:03:28 -0300

    fix(Page About): top background added in index file

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,11 +1,23 @@
+import styled from "styled-components"
 import { Footer, Header } from "../../Components"
 import { Contacts, Main, Skills } from "./Sections"
 
 export const About: React.FC = () => {
+    const BackgroundTop = styled.div`
+        height: 100%;
+        width: 100%;
+        max-width: 1920px;
+        z-index: -1;
+        background: ${({ theme }) => theme.colors.secondary500}; 
+        padding-block-end: 50px;
+    `
+
     return (
         <>
-            <Header />
-            <Main />
+            <BackgroundTop>
+                <Header />
+                <Main />
+            </BackgroundTop>
             <Skills />
             <Contacts />
             <Footer />
