d2d1b961bead8a251eb22bb941f6c75468c5c197
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-28 23:46:09 -0300

    chore: added Title component exporter file

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Footer } from './Footer'
 export { default as Forms } from './Forms'
 export { default as Header } from './Header/index'
+export { default as Title } from './Title/index'
