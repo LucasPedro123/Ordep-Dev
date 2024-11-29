2f029d50a0db4a99e5f02540b22a4e0717fcbcf9
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-11-28 23:39:22 -0300

    refactor(Home): called Title component in Divider section

--- src/Pages/Home/Sections/Section-divider/index.tsx
+++ src/Pages/Home/Sections/Section-divider/index.tsx
@@ -1,4 +1,5 @@
 import { fadeInUp } from '../../../../animations/animations.motion'
+import { Title } from '../../../../Components'
 import { Button3 } from '../../../../Components/Button'
 import * as S from './style'
 
@@ -14,7 +15,7 @@ const SectionDivider = () => {
                 exit={fadeInUp.exit}
                 transition={fadeInUp.transition}
             >
-                <S.Title>Leve Suas Ideias para o <br/> Próximo Nível</S.Title>
+                <Title content='Leve Suas Ideias para o Próximo Nível' color='white'/>
                 <S.Description>Não importa onde você esteja, transformamos suas ideias em realidade digital, seja para um novo site, plataforma ou aplicativo. Com processos ágeis e comunicação direta, você acompanha cada etapa do desenvolvimento.</S.Description>
             </S.Wrapper>
             <Button3 link='/#contact' content='Comece Agora' />
